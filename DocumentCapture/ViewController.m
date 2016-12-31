4260b752615901d3acaf8d6e2168b0550d64cba1
mattmoncur <matty.moncur@gmail.com>
2016-12-31 16:24:18 -0500

    * using AVFoundation to detect if back camera is available. if device found and access if granted add the input to the AVCapture session. * using AVCapturePreviewLayer to simply display the output of the capture device.
    Note: will be splitting this to use AVCaptureVideoOutput and OpenGL.

--- DocumentCapture/ViewController.m
+++ DocumentCapture/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
 
 @interface ViewController ()
 
@@ -23,6 +25,53 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupImageCaptureDevice {
+    
+    AVCaptureSession *session = [AVCaptureSession new];
+    
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    
+    NSArray *deviceTypes = [NSArray arrayWithObject:AVCaptureDeviceTypeBuiltInWideAngleCamera];
+    AVCaptureDeviceDiscoverySession *discovery = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:deviceTypes
+                                                           mediaType:AVMediaTypeVideo
+                                                            position:AVCaptureDevicePositionBack];
+    
+    NSArray *devices = discovery.devices;
+    
+    
+    if (devices.count <= 0 && authStatus != AVAuthorizationStatusDenied && authStatus != AVAuthorizationStatusRestricted) {
+        NSLog(@"No devices found");
+        return;
+    }
+    
+    if (authStatus == AVAuthorizationStatusNotDetermined) {
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+            if (granted) {
+                //proceed
+            } else {
+                //Throw error or sum sum
+            }
+        }];
+    }
+    
+    
+    AVCaptureDevice *device = devices[0];
+    NSLog(@"%@", device.localizedName);
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+
+    //Add device input to Capture Session
+    if ([session canAddInput:input]) {
+        [session addInput:input];
+    }
+    
+    //Create Preview Layer for Capture Session and add as sublayer
+    AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    previewLayer.frame = self.view.bounds;
+    [self.view.layer addSublayer:previewLayer];
+    
 }
 
 
